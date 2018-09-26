e19b9d73d97a69273f8e8735b2e5508c31162230
Jerrar Bukhari <mjerrar28@live.com>
2018-09-25 21:07:42 -0400

    increased test coverage to 100%

--- app/main.cpp
+++ app/main.cpp
@@ -12,16 +12,15 @@
 #include <iostream>
 
 int main() {
-
   pidController pid;
-  pid.setParamkp(0.1);
-  pid.setParamki(0.2);
-  pid.setParamkd(0.3);
-  pid.setParamdt(0.1);
-  pid.setParampE(0.0);
+  pid.setParamkp(0.1);  ///< set proportional gain
+  pid.setParamki(0.2);  ///< set integral gain
+  pid.setParamkd(0.3);  ///< set derivative gain
+  pid.setParamdt(0.1);  ///< set time step interval
+  pid.setParampE(0.0);  ///< set previous error to zero
   std::cout << " kp = " << pid.getParamkp() << std::endl;
   std::cout << " ki = " << pid.getParamki() << std::endl;
   std::cout << " kd = " << pid.getParamkd() << std::endl;
-  double output = pid.compute(40, 35);
+  double output = pid.compute(40, 35);  ///< calculate pid output
   std::cout << " output = " << output << std::endl;
 }
--- app/pidController.cpp
+++ app/pidController.cpp
@@ -18,34 +18,66 @@ pidController::pidController()
       prevError(0) {
 }
 
+/**
+ * @brief get kp
+ * @return kp
+ */
 double pidController::getParamkp() {
   return kp;
 }
 
+/**
+ * @brief get ki
+ * @return ki
+ */
 double pidController::getParamki() {
   return ki;
 }
 
+/**
+ * @brief get kd
+ * @return kd
+ */
 double pidController::getParamkd() {
   return kd;
 }
 
+/**
+ * @brief set kp
+ * @param new kp
+ */
 void pidController::setParamkp(double kp_) {
   kp = kp_;
 }
 
+/**
+ * @brief set ki
+ * @param new ki
+ */
 void pidController::setParamki(double ki_) {
   ki = ki_;
 }
 
+/**
+ * @brief set kd
+ * @param new kd
+ */
 void pidController::setParamkd(double kd_) {
   kd = kd_;
 }
 
+/**
+ * @brief set dt time interval
+ * @param new dt
+ */
 void pidController::setParamdt(double dt_) {
   dt = dt_;
 }
 
+/**
+ * @brief set pE previous error
+ * @param pE
+ */
 void pidController::setParampE(double pE_) {
   prevError = pE_;
 }
@@ -53,6 +85,12 @@ void pidController::setParampE(double pE_) {
 pidController::~pidController() {
 }
 
+/**
+ * @brief Compute the actuation signal of the pidController
+ * @param setpoint the desired target to reach
+ * @param cV current velocity
+ * @return output the computed pid actuation signal
+ */
 double pidController::compute(double setPoint, double cV) {
   double error = 0.0;
   double output = 0.0;
